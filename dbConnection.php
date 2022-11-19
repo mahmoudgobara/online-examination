32c9aaf18923927e8f7fcca251819adf2dbe9f1d
Mahmoud Gobara <mmagobara@gmail.com>
2022-11-19 02:16:30 +0200

    first commit

---
+++ dbConnection.php
@@ -0,0 +1,12 @@
+<?php
+//all the variables defined here are accessible in all the files that include this one
+$con= new mysqli('localhost','root','','project');
+
+if($con){
+    
+}
+else{
+
+}
+
+?>
